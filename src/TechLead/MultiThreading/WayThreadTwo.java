fc4ac12f6f0b18def7dd82328db72daf65e5353b
Veerateja Gopireddy <veerateja.gopireddy@RealPage.com>
2022-02-03 17:46:54 +0530

    commit

---
+++ src/TechLead/MultiThreading/WayThreadTwo.java
@@ -0,0 +1,11 @@
+package TechLead.MultiThreading;
+
+public class WayThreadTwo implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
